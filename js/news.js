39675906896db8da5e5685f209c4e1cb9aa52794
Sadab <sadabrahman275@gmail.com>
2022-09-03 03:46:14 +0600

    categories id added

--- js/news.js
+++ js/news.js
@@ -8,14 +8,25 @@ const connectApi = () => {
 const showCategory = categories => {
     const categoryContainer = document.getElementById("categoryContainer");
     categories.forEach(category => {
+        // console.log(category);
         const containerFluid = document.createElement("div");
-        containerFluid.classList.add("container-fluid");
+        containerFluid.classList.add("col");
         containerFluid.innerHTML = `
-        <a class="nav-link">${category.category_name}</a>
+        <a onclick='displayNews("${category.category_id}")' class="nav-link">${category.category_name}</a>
         `;
         categoryContainer.appendChild(containerFluid);
     })
-
-
 }
+
+const displayNews = idNews => {
+    console.log(idNews);
+}
+// const displayNews = (idNews) => {
+//     const url = `https://openapi.programming-hero.com/api/news/category/${idNews}`
+
+//     fetch(url)
+//         .then(res => res.json())
+//         .then(data => showMealDetail(data.data[0]));
+// }
+// showMealDetail
 connectApi();
