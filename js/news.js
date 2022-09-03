9e188e31d12a4e305cc0b3cd5260bc57ef795bc3
Sadab <sadabrahman275@gmail.com>
2022-09-03 17:59:23 +0600

    cards added

--- js/news.js
+++ js/news.js
@@ -18,15 +18,38 @@ const showCategory = categories => {
     })
 }
 
-const displayNews = idNews => {
-    console.log(idNews);
-}
-// const displayNews = (idNews) => {
-//     const url = `https://openapi.programming-hero.com/api/news/category/${idNews}`
-
-//     fetch(url)
-//         .then(res => res.json())
-//         .then(data => showMealDetail(data.data[0]));
+// const displayNews = idNews => {
+//     console.log(idNews);
 // }
-// showMealDetail
+const displayNews = (idNews) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${idNews}`
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showNews(data.data));
+}
+// console.log
+const showNews = (news) => {
+    console.log(news[0]);
+    const newsDiv = document.getElementById("newsContainer");
+    newsDiv.innerHTML = ``;
+    news.forEach(newss => {
+
+        const newsBox = document.createElement("div");
+        newsBox.classList.add("row");
+        newsBox.innerHTML = `
+        <div class="col-md-4">
+                        <img src="${newss.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">${newss.title}</h5>
+                            <p class="card-text">${newss.details.slice(0, 300) + "..."}</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+        `
+        newsDiv.appendChild(newsBox);
+    })
+}
 connectApi();
